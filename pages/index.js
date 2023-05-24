52e968a5a58fb6bfc759ccf05b06e6c149d08b5e
Igor Lopes <89506748+Ilopesr@users.noreply.github.com>
2023-05-24 01:24:32 +0000

    Adicionando pages'

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+
+    return (
+        <div>
+            <h1>Oi amor da minha vida.</h1>
+            <p>Obrigado por me apoiar em tudo que eu faço.</p>
+            <small>ps: Igor Lopes</small>
+        </div>
+        
+    )
+}
+
+export default Home;
