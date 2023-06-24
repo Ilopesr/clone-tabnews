d6f8f025befd07a17ca0d1c75c135febe82c4a45
Igor Lopes <89506748+Ilopesr@users.noreply.github.com>
2023-06-24 21:42:25 +0000

    adicionado scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 function Home() {
+  return (
+    <div>
+      <h1>Oi amor da minha vida.</h1>
+      <p>Obrigado por me apoiar em tudo que eu faço.</p>
+      <div>
+        <small>ps: Igor Lopes</small>
+        <p>
+          <small>
+            <small>Leva eu pra faculdade, ta frio.</small>
+          </small>
+        </p>
+      </div>
+    </div>
 
-    return (
-        <div>
-            <h1>Oi amor da minha vida.</h1>
-            <p>Obrigado por me apoiar em tudo que eu faço.</p>
-            <div>
-                <small>ps: Igor Lopes</small>
-                <p>
-                    <small>
-                        <small>Leva eu pra faculdade, ta frio.</small>
-                    </small>
-                </p>
-            </div>
-        </div>
-        
-    )
+
+  )
 }
 
 export default Home;
