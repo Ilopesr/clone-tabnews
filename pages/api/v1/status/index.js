fa492b2c7fe85387e620c72e4dadd4a4a1151810
Igor Lopes <89506748+Ilopesr@users.noreply.github.com>
2025-01-10 18:16:36 +0000

    add `database.js` and `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
+import database from "../../../../infra/database";
+
+
 async function status(request, response) {
+  const result = await database.query("SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'postgres';");
   response.status(200).json({ status: "OK" })
 }
 
