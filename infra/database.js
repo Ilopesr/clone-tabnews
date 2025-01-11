9bd78acc0abef2a1e5a3485113845899cb39229b
ilopesr <igor.lop.r@gmail.com>
2025-01-10 21:56:53 -0300

    added `node-pg-migration` and scripts

--- infra/database.js
+++ infra/database.js
@@ -24,14 +24,6 @@ async function getNewClient() {
     ssl: getSslValue(),
   });
 
-  console.log("Credênciais do postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSslValue(),
-  });
   await client.connect();
   return client;
 }
